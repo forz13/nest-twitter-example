6778a35bd7211ba3f15dbd1e6ef72360e009a09e
forz <kalashnikov.sergey13@gmail.com>
2019-12-22 13:10:05 +0400

    add lock file

--- dist/migrations/1576828969154-AAA.d.ts
+++
@@ -1,5 +0,0 @@
-import { MigrationInterface, QueryRunner } from 'typeorm';
-export declare class AAA1576828969154 implements MigrationInterface {
-    up(queryRunner: QueryRunner): Promise<any>;
-    down(queryRunner: QueryRunner): Promise<any>;
-}
